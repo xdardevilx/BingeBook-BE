e0d52d3da16f6367bdbb7cb5f4fdb15284f55511
xdardevilx <valeriogalletti21@gmail.com>
2024-04-30 20:07:33 +0200

    init

---
+++ src/main/java/valerio/BingeBookBE/BingeBookBeApplication.java
@@ -0,0 +1,13 @@
+package valerio.BingeBookBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingeBookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingeBookBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/valerio/BingeBookBE/BingeBookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package valerio.BingeBookBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingeBookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
